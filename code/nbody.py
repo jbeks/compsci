3eb6cb79bbed51acbad1cff7b620a262c788edf8
Gawan Dekker <gawan.dekker@gmail.com>
2018-01-15 13:11:55 +0100

    Added more integration methods, fixed Verlet integration

--- code/nbody.py
+++ code/nbody.py
@@ -38,7 +38,7 @@ class Body:
     def calculate_update(self, sys, dt, G):
         assert False, "Implement in child class"
     def confirm_update(self):
-        assert False, "Implement in child class"
+        self.p = self.p_tmp
     def set_e0(self, sys, G):
         self.e0 = self.get_ek(sys) + self.get_ep(sys, G)
     def get_ek(self, sys):
@@ -49,8 +49,8 @@ class Body:
             if b == self:
                 continue
             vector = b.p - self.p
-            ep -= G * b.m / np.linalg.norm(vector)
-        return ep * self.m
+            ep -= b.m / np.linalg.norm(vector)
+        return ep * self.m * G
 
 class System:
     def __init__(self, sys, G):
@@ -70,18 +70,22 @@ class System:
         for b in self.sys:
             b.confirm_update()
             break                                                       #TODO
-    def print_energy(self, t):
+    def print_energy(self, t, n):
         for b in self.sys:
             ek = b.get_ek(self.sys)
             ep = b.get_ep(self.sys, self.G)
             etot = ek + ep
-            print('t          ', t)
-            print('ek         ', ek)
-            print('ep         ', ep)
-            print('etot       ', etot)
-            print('e0         ', b.e0)
-            print('etot - e0  ', etot - b.e0)
-            print('etot-e0/e0 ', (etot - b.e0) / b.e0)
+            print('at time t =',t,', after',n,'steps :')
+            print('  E_kin =',ek,', E_pot =',ep,', E_tot =',etot)
+            print('             E_tot - E_init = ',etot-b.e0)
+            print('  (E_tot - E_init) / E_init =', (etot - b.e0) / b.e0)
+#            print('t          ', t)
+#            print('ek         ', ek)
+#            print('ep         ', ep)
+#            print('etot       ', etot)
+#            print('e0         ', b.e0)
+#            print('etot - e0  ', etot - b.e0)
+#            print('etot-e0/e0 ', (etot - b.e0) / b.e0)
             break                                                       #TODO
 
 class Body_Euler(Body):
@@ -89,37 +93,80 @@ class Body_Euler(Body):
         a = self.acceleration(sys, G)
         self.p_tmp = self.p + self.v * dt
         self.v = self.v + a * dt
-    def confirm_update(self):
-        self.p = self.p_tmp
 
 class Body_Verlet(Body):
     def calculate_update(self, sys, dt, G):
-        a = self.acceleration(sys, G)
-        self.v = self.v + 0.5 * a * dt
-        self.p_tmp = self.p + self.v * dt
-    def confirm_update(self):
-        self.p = self.p_tmp
+        p0 = self.p
+        v0 = self.v
+        a0 = self.acceleration(sys, G)
+        self.v = self.v + 0.5 * a0 * dt
+        self.p = self.p + self.v * dt
+        a1 = self.acceleration(sys, G)
+        self.v = v0 + 0.5 * (a0 + a1) * dt
+        self.p_tmp = self.p
+        self.p = p0
 
-class System_Verlet(System):
-    def step(self, dt):
-        for _ in range(2):
-            super().step(dt)
+# TODO next Bodies copied from artcopsci, change to fit code more
+#      might not work when updating multiple bodies
+
+class Body_Leapfrog(Body):
+    def calculate_update(self, sys, dt, G):
+        p0 = self.p
+        a0 = self.acceleration(sys, G)
+        self.p = self.p + self.v * dt + .5 * a0 * dt * dt
+        a1 = self.acceleration(sys, G)
+        self.v = self.v + .5 * (a0 + a1) * dt
+        self.p_tmp = self.p
+        self.p = p0
+
+class Body_RK2(Body):
+    def calculate_update(self, sys, dt, G):
+        p0 = self.p
+        v0 = self.v + .5 * a0 * dt
+        a0 = self.acceleration(sys, G)
+        self.p = self.p + .5 * self.v * dt
+        a1 = self.acceleration(sys, G)
+        self.v = self.v + a1 * dt
+        self.p = p0 + v0 * dt
+        self.p_tmp = self.p
+        self.p = p0
+
+class Body_RK4(Body):
+    def calculate_update(self, sys, dt, G):
+        p0 = self.p
+        a0 = self.acceleration(sys, G)
+        self.p = p0 + .5 * self.v * dt + .125 * a0 * dt * dt
+        a1 = self.acceleration(sys, G)
+        self.p = p0 + self.v * dt + 0.5 * a1 * dt * dt
+        a2 = self.acceleration(sys, G)
+        self.p = p0 + self.v * dt + (1 / 6.) * (a0 + 2 * a1) * dt * dt
+        self.v = self.v + (1 / 6.) * (a0 + 4 * a1 + a2) * dt
+        self.p_tmp = self.p
+        self.p = p0
+
+#class System_Verlet(System):
+#    def step(self, dt):
+#        for _ in range(2):
+#            super().step(dt)
 
 def simulate(sys, dt, t_end, dt_out=-1, dt_dia=-1, show_plot=False):
-    t = 0
-    t_out = dt_out -.5 * dt
-    t_dia = dt_dia -.5 * dt
+    n = 0
+    t =  .5 * dt
+    t_out = dt_out
+    t_dia = dt_dia
 
     if dt_dia > 0:
-        sys.print_energy(t_dia)
+        sys.print_energy(0, n)
 
     lst = [[x.p for x in sys.sys]]
     while t < t_end:
+
         sys.step(dt)
         t += dt
+        n += 1
 
         if dt_dia > 0 and t >= t_dia:
-            sys.print_energy(t_dia)
+            sys.print_energy(t_dia, n)
             t_dia += dt_dia
 
         if dt_out > 0 and t >= t_out:
@@ -158,13 +205,25 @@ if __name__ == "__main__":
     assert itype in [
         "euler",
         "verlet",
+        "leapfrog",
+        "rk2",
+        "rk4",
     ], "Integration type not supported"
-    if itype == "verlet":
-        params = get_params(Body_Verlet)
-        system = System_Verlet(*params[:2])
-    else:
-        if itype == "euler":
-            params = get_params(Body_Euler)
-        system = System(*params[:2])
+#    if itype == "verlet":
+#        params = get_params(Body_Verlet)
+#        system = System_Verlet(*params[:2])
+#    else:
+    if itype == "euler":
+        btype = Body_Euler
+    elif itype == "verlet":
+        btype = Body_Verlet
+    elif itype == "leapfrog":
+        btype = Body_Leapfrog
+    elif itype == "rk2":
+        btype = Body_RK2
+    elif itype == "rk4":
+        btype = Body_RK4
+    params = get_params(btype)
+    system = System(*params[:2])
     simulate(system, *params[2:], False)
 
