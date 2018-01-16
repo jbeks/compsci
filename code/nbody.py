d9fd147a7557e3d70c7e53c65af30daa62640c0f
Gawan Dekker <gawan.dekker@gmail.com>
2018-01-16 17:07:25 +0100

    Moved integration into System class, added loops around every integration line for more precision

--- code/nbody.py
+++ code/nbody.py
@@ -16,67 +16,6 @@ def simple_plot(xs, ys, type=0):
 #    plt.xlim([-2,12])
     plt.show()
 
-def acceleration(p, sys, G):
-    a = 0
-    for b in sys:
-        vector = b.p - p
-        a += vector * b.m / float(np.linalg.norm(vector) ** 3)
-    return a * G
-
-def jerk(p, v, sys, G):
-    j = 0
-    for b in sys:
-        p_vector = b.p - p
-        v_vector = b.v - v
-        len_p_vector = np.linalg.norm(p_vector)
-        j += b.m * (v_vector / float(len_p_vector ** 3)
-            - 3 * np.dot(p_vector, v_vector) * p_vector / len_p_vector ** 5)
-    return j * G
-
-class Integration:
-    def euler(dt, p0, v0, sys, G):
-        afunc = lambda p: acceleration(p, sys, G)
-        a0 = afunc(p0)
-        p1 = p0 + v0 * dt
-        v1 = v0 + a0 * dt
-        return p1, v1
-    def verlet(dt, p0, v0, sys, G):
-        afunc = lambda p: acceleration(p, sys, G)
-        a0 = afunc(p0)
-        p1 = p0 + v0 * dt + 0.5 * a0 * dt * dt
-        a1 = afunc(p1)
-        v1 = v0 + 0.5 * (a0 + a1) * dt
-        return p1, v1
-    def rk2(dt, p0, v0, sys, G):
-        afunc = lambda p: acceleration(p, sys, G)
-        a0 = afunc(p0)
-        v0_5 = v0 + 0.5 * a0 * dt
-        p0_5 = p0 + 0.5 * v0 * dt
-        a0_5 = afunc(p0_5)
-        v1 = v0 + a0_5 * dt
-        p1 = p0 + v0_5 * dt
-        return p1, v1
-    def rk4(dt, p0, v0, sys, G):
-        afunc = lambda p: acceleration(p, sys, G)
-        k1 = afunc(p0) * dt
-        k2 = afunc(p0 + 0.5 * v0 * dt + 0.125 * k1 * dt) * dt
-        k3 = afunc(p0 + v0 * dt + 0.5 * k2 * dt) * dt
-        p1 = p0 + v0 * dt + (1 / 6.0) * (k1 + 2 * k2) * dt
-        v1 = v0 + (1 / 6.0) * (k1 + 4 * k2 + k3)
-        return p1, v1
-    def hermite(dt, p0, v0, sys, G):
-        afunc = lambda p: acceleration(p, sys, G)
-        jfunc = lambda p, v: jerk(p, v, sys, G)
-        a0 = afunc(p0)
-        j0 = jfunc(p0, v0)
-        p1_p = p0 + v0 * dt + 0.5 * a0 * dt * dt + (1 / 6.0) * j0 * dt * dt *dt
-        v1_p = v0 + a0 * dt + 0.5 * j0 * dt * dt
-        a1_p = afunc(p1_p)
-        j1_p = jfunc(p1_p, v1_p)
-        v1 = v0 + 0.5 * (a0 + a1_p) * dt + (1 / 12.0) * (j0 - j1_p) * dt * dt
-        p1 = p0 + 0.5 * (v0 + v1) * dt + (1 / 12.0) * (a0 - a1_p) * dt * dt
-        return p1, v1
-
 class Body:
     def __init__(self, m, p, v):
         assert len(p) == 3, "Insufficient location parameters"
@@ -84,21 +23,21 @@ class Body:
         self.m = float(m)
         self.p = np.array(p)
         self.v = np.array(v)
-        self.p_tmp = np.zeros(3)
-        self.v_tmp = np.zeros(3)
-        self.e0 = 0
+        self.a = np.zeros(3)
+        self.j = np.zeros(3)
+#        self.p_tmp = np.zeros(3)
+#        self.v_tmp = np.zeros(3)
     def __repr__(self):
         return "({}, {}, {})".format(self.m, self.p, self.v)
-    def set_update(self, p, v):
-        self.p_tmp = p
-        self.v_tmp = v
-    def confirm_update(self):
-        self.p = self.p_tmp
-        self.v = self.v_tmp
-    def set_e0(self, sys, G):
-        self.e0 = self.get_ek(sys) + self.get_ep(sys, G)
-    def get_ek(self, sys):
-        return .5 * self.m * np.linalg.norm(self.v) ** 2
+#    def set_update(self, p, v):
+#        self.p_tmp = p
+#        self.v_tmp = v
+#    def confirm_update(self):
+#        self.p = self.p_tmp
+#        self.v = self.v_tmp
+    def get_ek(self):
+        # TODO removed * self.m
+        return .5 * np.linalg.norm(self.v) ** 2
     def get_ep(self, sys, G):
         ep = 0
         for b in sys:
@@ -107,14 +46,54 @@ class Body:
             vector = b.p - self.p
             ep -= b.m / np.linalg.norm(vector)
         return ep * self.m * G
+    def set_p(self, p):
+        self.p = p
+    def set_v(self, v):
+        self.v = v
+    def calc_a(self, sys, G):
+        self.a = np.zeros(3)
+        for b in sys:
+            if b == self:
+                continue
+            vector = b.p - self.p
+            self.a += vector * b.m / float(np.linalg.norm(vector) ** 3)
+        self.a *= G
+    def calc_j(self, sys, G):
+        self.j = np.zeros(3)
+        for b in sys:
+            if b == self:
+                continue
+            p_vector = b.p - self.p
+            v_vector = b.v - self.v
+            len_p_vector = np.linalg.norm(p_vector)
+            self.j += b.m * (v_vector / float(len_p_vector ** 3)
+                - 3 * np.dot(p_vector, v_vector) * p_vector / len_p_vector ** 5)
+        self.j *= G
 
 class System:
-    def __init__(self, sys, G, ifunc):
+    def __init__(self, sys, G, itype):
         self.G = G
         self.sys = sys
-        self.ifunc = ifunc
-        for b in sys:
-            b.set_e0(sys, G)
+        self.e0 = self.get_ek() + self.get_ep()
+        self.ifunc = None
+        assert itype in [
+            "euler",
+            "verlet",
+            "leapfrog",
+            "rk2",
+            "rk4",
+            "hermite",
+        ], "Integration type not supported"
+        if itype == "euler":
+            self.ifunc = self.euler
+        elif itype == "verlet" or itype == "leapfrog":
+            self.ifunc = self.verlet
+        elif itype == "rk2":
+            self.ifunc = self.rk2
+        elif itype == "rk4":
+            self.ifunc = self.rk4
+        elif itype == "hermite":
+            self.ifunc = self.hermite
     def __repr__(self):
         s = ''
         for b in self.sys:
@@ -122,33 +101,108 @@ class System:
         return s[:-1]
     # make sure sys does not contain the body with position p
     def step(self, dt):
+        self.ifunc(dt)
+    def get_ek(self):
+        ek = 0
         for b in self.sys:
-            b.set_update(
-                *self.ifunc(
-                    dt, b.p, b.v, [x for x in self.sys if x != b], self.G
-                )
-            )
-#            break                                                       #TODO
+            ek += b.get_ek()
+        return ek
+    def get_ep(self):
+        ep = 0
         for b in self.sys:
-            b.confirm_update()
-#            break                                                       #TODO
+            ep += b.get_ep(self.sys, self.G)
+        return 0.5 * ep
     def print_energy(self, t, n):
+        ek = self.get_ek()
+        ep = self.get_ep()
+        etot = ek + ep
+        print('at time t =',t,', after',n,'steps :')
+        print('  E_kin =',ek,', E_pot =',ep,', E_tot =',etot)
+        print('             E_tot - E_init = ',etot-self.e0)
+        print('  (E_tot - E_init) / E_init =', (etot - self.e0) / self.e0)
+    def euler(self, dt):
+        for b in self.sys: b.calc_a(self.sys, self.G); break
+        for b in self.sys: b.set_p(b.p + b.v * dt); break
+        for b in self.sys: b.set_v(b.v + b.a * dt); break
+    def verlet(self, dt):
+        for b in self.sys: b.calc_a(self.sys, self.G); break
+        for b in self.sys: b.set_v(b.v + .5 * b.a * dt); break
+        for b in self.sys: b.set_p(b.p + b.v * dt); break
+        for b in self.sys: b.calc_a(self.sys, self.G); break
+        for b in self.sys: b.set_v(b.v + .5 * b.a * dt); break
+    def rk2(self, dt):
+        p0 = []
+        v0_5 = []
+        for b in self.sys: b.calc_a(self.sys, self.G); break
         for b in self.sys:
-            ek = b.get_ek(self.sys)
-            ep = b.get_ep(self.sys, self.G)
-            etot = ek + ep
-            print('at time t =',t,', after',n,'steps :')
-            print('  E_kin =',ek,', E_pot =',ep,', E_tot =',etot)
-            print('             E_tot - E_init = ',etot-b.e0)
-            print('  (E_tot - E_init) / E_init =', (etot - b.e0) / b.e0)
-#            print('t          ', t)
-#            print('ek         ', ek)
-#            print('ep         ', ep)
-#            print('etot       ', etot)
-#            print('e0         ', b.e0)
-#            print('etot - e0  ', etot - b.e0)
-#            print('etot-e0/e0 ', (etot - b.e0) / b.e0)
-            break                                                       #TODO
+            p0.append(b.p)
+            v0_5.append(b.v + .5 * b.a * dt)
+            break
+        for b in self.sys: b.set_p(b.p + .5 * b.v * dt); break
+        for b in self.sys: b.calc_a(self.sys, self.G); break
+        for b in self.sys: b.set_v(b.v + b.a * dt); break
+        for i in range(len(self.sys)):
+            self.sys[i].set_p(p0[i] + v0_5[i] * dt)
+            break
+    def rk4(self, dt):
+        p0 = []
+        v0 = []
+        k1 = []
+        k2 = []
+        k3 = []
+        for b in self.sys:
+            p0.append(b.p)
+            v0.append(b.v)
+            break
+        for b in self.sys:
+            b.calc_a(self.sys, self.G)
+            k1.append(b.a * dt)
+            break
+        for i in range(len(self.sys)):
+            self.sys[i].set_p(p0[i] + .5 * v0[i] * dt + .125 * k1[i] * dt)
+            self.sys[i].calc_a(self.sys, self.G)
+            k2.append(b.a * dt)
+            break
+        for i in range(len(self.sys)):
+            self.sys[i].set_p(p0[i] + v0[i] * dt + .5 * k2[i] * dt)
+            self.sys[i].calc_a(self.sys, self.G)
+            k3.append(b.a * dt)
+            break
+        for i in range(len(self.sys)):
+            self.sys[i].set_p(p0[i] + v0[i] * dt + (k1[i] + 2*k2[i]) * dt / 6.)
+            self.sys[i].set_v(v0[i] + (k1[i] + 4*k2[i] + k3[i]) / 6.)
+            break
+    def hermite(self, dt):
+        p0 = []
+        v0 = []
+        a0 = []
+        j0 = []
+        for b in self.sys:
+            p0.append(b.p)
+            v0.append(b.v)
+            break
+        for b in self.sys:
+            b.calc_a(self.sys, self.G)
+            b.calc_j(self.sys, self.G)
+            a0.append(b.a)
+            j0.append(b.j)
+            break
+        for b in self.sys:
+            b.set_p(b.p + b.v * dt + .5 * b.a * dt*dt + b.j * dt*dt*dt / 6.)
+            b.set_v(b.v + b.a * dt + .5 * b.j * dt*dt)
+            break
+        for b in self.sys:
+            b.calc_a(self.sys, self.G)
+            b.calc_j(self.sys, self.G)
+            break
+        for i in range(len(self.sys)):
+            self.sys[i].set_v(v0[i] + .5 * (a0[i] + self.sys[i].a) * dt \
+                + (j0[i] - self.sys[i].j) * dt*dt / 12.)
+            break
+        for i in range(len(self.sys)):
+            self.sys[i].set_p(p0[i] + .5 * (v0[i] + self.sys[i].v) * dt \
+                + (a0[i] - self.sys[i].a) * dt*dt / 12.)
+            break
 
 def simulate(system, dt, t_end, dt_out=-1, dt_dia=-1, show_plot=False):
     n = 0
@@ -203,25 +257,25 @@ def get_params():
 if __name__ == "__main__":
     assert len(sys.argv) == 2, "Insufficient amount of arguments"
     itype = sys.argv[1].lower()
-    assert itype in [
-        "euler",
-        "verlet",
-        "leapfrog",
-        "rk2",
-        "rk4",
-        "hermite",
-    ], "Integration type not supported"
-    if itype == "euler":
-        ifunc = Integration.euler
-    elif itype == "verlet" or itype == "leapfrog":
-        ifunc = Integration.verlet
-    elif itype == "rk2":
-        ifunc = Integration.rk2
-    elif itype == "rk4":
-        ifunc = Integration.rk4
-    elif itype == "hermite":
-        ifunc = Integration.hermite
+#    assert itype in [
+#        "euler",
+#        "verlet",
+#        "leapfrog",
+#        "rk2",
+#        "rk4",
+#        "hermite",
+#    ], "Integration type not supported"
+#    if itype == "euler":
+#        ifunc = Integration.euler
+#    elif itype == "verlet" or itype == "leapfrog":
+#        ifunc = Integration.verlet
+#    elif itype == "rk2":
+#        ifunc = Integration.rk2
+#    elif itype == "rk4":
+#        ifunc = Integration.rk4
+#    elif itype == "hermite":
+#        ifunc = Integration.hermite
     params = get_params()
-    system = System(*params[:2], ifunc)
-    simulate(system, *params[2:], True)
+    system = System(*params[:2], itype)
+    simulate(system, *params[2:], False)
 
