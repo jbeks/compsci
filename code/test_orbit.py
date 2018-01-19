88fa63b337c6150b21e0275ff826d26f87cf8ae5
Jetske Beks <jetske151@hotmail.com>
2018-01-19 17:37:12 +0100

    whoops foutjes in berekening gefixt etc

--- code/test_orbit.py
+++ code/test_orbit.py
@@ -7,6 +7,10 @@ def v_error(data, index):
     return np.linalg.norm(data[0]) - np.linalg.norm(data[index])
 
 def pos_data(data, orbit_i):
+    # elke baan is een ellips
+    # ellips heeft formule
+    # elke volle orbit: fit de ellips
+    # bereken verschil tussen elke ellips (lstsq?)
     return
 
 def find_dists(data_body, data_center):
@@ -36,11 +40,11 @@ def orbit_data(data, center):
     orbit_i = 0
     orbit_err = []
     while orbit_i < len(data) and orbit_i != -1:
-        orbit_i = find_period(data, center, orbit_i)
         orbit_err.append(orbit_i)
-    avg_err = abs(orbit_err[0] - np.mean(orbit_err))
-    max_err = abs(orbit_err[0] - max(orbit_err))
-    return orbit_err[0], avg_err, max_err, orbit_err
+        orbit_i = find_period(data, center, orbit_i)
+    orbit_diff = (np.roll(orbit_err, -1) - orbit_err)[:-1]
+    print(orbit_diff)
+    return (orbit_err[1] if len(orbit_err) > 1 else -1), orbit_err, orbit_diff
 
 def evaluation(data, v_data, dt, system):
     find = lambda x: next((i for i in range(len(system)) if system[i].name == x.parent), None)
@@ -48,25 +52,25 @@ def evaluation(data, v_data, dt, system):
         body = system[i]
         p_index = find(body)
         mi, ma = find_dists(data[i], data[p_index])
-        orbit_i, avg_err, max_err, points = orbit_data(data[i], data[p_index])
+        orbit_i, points, diff = orbit_data(data[i], data[p_index])
         p = orbit_i * dt / 86400.
         pos_diff = pos_data(data, points)
-        p_err = np.linalg.norm(data[i][orbit_i] - data[i][0]) if orbit_i != 0 else -1
-        v_err = v_error(v_data[i], orbit_i)
+        p_err = np.linalg.norm(data[i][orbit_i] - data[i][0]) if orbit_i != -1 else -1
+        v_err = v_error(v_data[i], orbit_i) if orbit_i != -1 else -1
         print("Body " + str(i) + " (" + str(system[i]) + "):")
         print("  min dist to " + str(system[p_index]) + " = " + str(mi) + " (km)")
         print("  max dist to " + str(system[p_index]) + " = " + str(ma) + " (km)")
         if p > 0:
-            print("  period of orbit: first " + str(p) + " and avg " + str(np.mean(points) * dt / 84600.) + " (days)")
-            print("  std: " + str(np.std(points) * dt / 84600.) + " (s) and max: " + str(max(points) * dt / 84600.) + " (s) and min: " + str(min(points) * dt / 84600.) + " (s)")
-            print("  orbit avg error: " + str(avg_err) + " (s) and max error " + str(max_err) + " (s) in " + str(len(points)) + " data points")
+            print("  period of orbit: first " + str(p) + " and avg " + str(np.mean(diff) * dt / 86400.) + " (days)")
+            print("  std: " + str(np.std(diff) * dt / 86400.) + " (s) and max: " + str(max(diff) * dt / 86400.) + " (s) and min: " + str(min(diff) * dt / 86400.) + " (s)")
+            print("and last: " + str(diff[-1] * dt / 86400.) + " in " + str(len(points)) + " data points")
         else:
             print("  period of orbit not found")
         print("  positional error = " + str(p_err) + " (km)")
         print("  velocity error = " + str(v_err) + " (km/s)")
 
 if __name__ == "__main__":
-    orbit = {}
+    orbit = {'Mercury': 87.97, 'Venus': 224.70, 'Earth': 365.25, 'Moon': 27.32, 'Mars': 686.98, 'Phobos': 0.3189}
     itype, t_end, dt, t_dia, t_out, plot, planet, years = parse_arguments()
     if t_end == 1:
         t_end = years * orbit[planet] * 24 * 60 * 60
