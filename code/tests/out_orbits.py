be1d821503d496a0c60ddac03b0846bb9cd12a5a
Jetske Beks <jetske151@hotmail.com>
2018-02-04 23:03:45 +0100

    commented out_orbits and convert_data

--- code/tests/out_orbits.py
+++ code/tests/out_orbits.py
@@ -7,31 +7,50 @@ import sys
 import code_dir
 from read_short_sim_data import read_short_sim_data
 
+# out_orbits.py:
+# compares a simulation with black hole
+# to one without and checks the differences
+# between the orbits of the planets
+# and whether they have detached from orbit
+
 def unit_vector(vector):
     """ Returns the unit vector of the vector.  """
     return vector / np.linalg.norm(vector)
 
 def angle(current, last):
-    v1_u = unit_vector(current)
-    v2_u = unit_vector(last)
-    return math.degrees(np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0)))
+    """ Calculates the angle between two vectors. """
+    u1 = unit_vector(current)
+    u2 = unit_vector(last)
+    return math.degrees(np.arccos(np.clip(np.dot(u1, u2), -1.0, 1.0)))
 
 def find_period(data_body, data_center, start):
+    """
+    Find the timestep where the body 'data_body' orbiting
+    around 'data_center', starting on timestep 'start,
+    has made a full orbit.
+    """
     data = data_body - data_center
     prev_dist = np.linalg.norm(data[start + 1] - data[start])
     orig = prev_dist
     back = False
+    # loop through the body datapoints per timestep
     for i in range(start + 2, len(data)):
         cur_dist = np.linalg.norm(data[i] - data[start])
+        # check when orbit is halfway done
         if not back:
             if cur_dist < prev_dist:
                 back = True
-        elif cur_dist > prev_dist: # and angle(cur_dist, orig) < 30:
+        # return timestep when orbit is done
+        elif cur_dist > prev_dist:
             return i
         prev_dist = cur_dist
     return -1
 
 def orbit_data(data, center):
+    """
+    Find the orbit locations in all the datapoints
+    of body 'data' orbiting around body 'center'.
+    """
     orbit_i = 0
     orbit_err = []
     while orbit_i < len(data) - 1 and orbit_i != -1:
@@ -40,6 +59,10 @@ def orbit_data(data, center):
     return orbit_err
 
 def phelions(points, center_points):
+    """
+    Find the aphelion and perihelion (smallest and biggest
+    distance to the orbit center) of all bodies.
+    """
     min_d = np.linalg.norm(points[0] - center_points[0])
     max_d = 0
     for i in range(1, len(points)):
@@ -51,16 +74,17 @@ def phelions(points, center_points):
     return max_d, min_d
 
 def no_orbit(planet_data, center_data, i, ori_pheli):
+    """ Check whether a planet has detached from orbit. """
     aphelion_check = (np.linalg.norm(planet_data[i] - center_data[i]) > 2 * ori_pheli)
     angle_check = True
     if i > 300:
-        # TODO velocities, not positions
         old_position = planet_data[i - 300] - planet_data[i - 301]
         new_position = planet_data[i] - planet_data[i-1]
         angle_check = (angle(old_position, new_position) < 0.5)
     return aphelion_check and angle_check
 
 def out_orbit_time(orbit_num, planet_data, center_data, ori_pheli, dt):
+    """ Find the timestep of detachment of orbit for body 'planet_data'. """
     orbit_indices = orbit_data(planet_data, center_data)
     for i in range(orbit_indices[orbit_num], len(planet_data)):
         if no_orbit(planet_data, center_data, i, ori_pheli[orbit_num][0]):
@@ -68,6 +92,10 @@ def out_orbit_time(orbit_num, planet_data, center_data, ori_pheli, dt):
     return -1
 
 def orbit_sta(new_phelis, ori_pheli, planet_data, center_data, dt):
+    """
+    Check the amount of orbits of body 'planet_data' with the reference simulation
+    and if this amount differs, find out whether the planet has detached.
+    """
     stabil = []
     if len(ori_pheli) > len(new_phelis):
         orbit_num = len(new_phelis)
@@ -100,24 +128,30 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
+    # read in the simulation data
     time, _, sim_data = read_short_sim_data(args.file)
     sim_data = np.array(sim_data)
     phel_list = []
+    # if there is no reference simulation file, create it
     if args.initialization:
         sim_data_subset = [
             sim_data[i] for i in range(len(sim_data))
             if i != (args.sun_index % len(sim_data))
         ]
+    # run the tests and find out the differences in orbit
     else:
         sim_data_subset = [
             sim_data[i] for i in range(len(sim_data))
             if i != (args.sun_index % len(sim_data))
             and i != (args.black_hole_index % len(sim_data))
         ]
+    # go through all orbiting bodies
     for planet in sim_data_subset:
         cur_phel = []
+        # find the timesteps of completed orbits
         orbit_list = orbit_data(planet, sim_data[args.sun_index])
         print("orbits: " + str(len(orbit_list) - 1))
+        # find the aphelion and perihelion for every orbit
         for i in range(len(orbit_list) - 1):
             cur_phel.append(
                 (
@@ -129,13 +163,17 @@ if __name__ == "__main__":
                 )
             )
         phel_list.append(cur_phel)
+
     sep = "\\" if os.name == "nt" else "/"
     outdir = os.path.abspath(
         os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
     ) + sep + "output" + sep + "out"
     reff = outdir + sep + "ori_list_data"
+
+    # save the found datapoints if it is initialisation
     if args.initialization:
         np.save(reff, phel_list)
+    # else compare the datapoints with the reference
     else:
         ori_list = np.load(reff + ".npy")
         orbit_stabi = []
--- convert_data.py
+++ convert_data.py
@@ -1,21 +1,34 @@
 import argparse
 
+# convert_data.py:
+# reads in the NASA data files
+# and outputs them in a more easily
+# readable format to test with
+
 def read_ref(name):
+    """
+    Reads a data reference file
+    with name '<name>_data.ref'
+    """
     sep = 0
     data = []
     ax = ["X", " Y", " Z"]
     newfile = open("ref/" + name + ".ref", 'w+')
+    # open the reference file
     with open("ref/" + name + "_data.ref", 'r') as f:
         for line in f:
+            # read and write the stepsize
             if line.startswith("Step-size"):
                 split = line.split()
                 stepsize = int(split[2]) * 60
                 newfile.write("stepsize " + str(stepsize) + "\n")
+            # only read data lines
             if "A.D." not in line:
                 continue
             s = line.split(",")
             if len(s) < 8:
                 continue
+            # write data in space-separated format
             newfile.write(s[2] + " " + s[3] + " " + s[4])
             newfile.write("\n")
     newfile.close()
